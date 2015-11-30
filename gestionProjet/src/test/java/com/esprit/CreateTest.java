fe879cb07e9d2d463bbda4ebbc6a7caec20bd9be
Mhamed <Mhamed@172.16.7.80>
2015-11-30 02:51:48 +0100

    Signed-off-by: Mhamed <Mhamed@172.16.7.80>

--- gestionProjet/src/test/java/com/esprit/CreateTest.java
+++ gestionProjet/src/test/java/com/esprit/CreateTest.java
@@ -24,7 +24,7 @@ import edu.esprit.gestion_projet.services.IService.IUserManager;
 //tests mahdi
 //sonarLunch2 /// /// 20/11/2015
 //emailTEST
-
+//charfis
 //radhihouli EMAIL SUCCESS
 @RunWith(MockitoJUnitRunner.class)
 public class CreateTest {
